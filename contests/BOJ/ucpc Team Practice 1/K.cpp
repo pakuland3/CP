b2e4d57984656abe920f131d5d5a7b6cfe901ea1
pakuland3 <pakuland_1@naver.com>
2024-07-06 15:02:39 +0900

    0706

---
+++ contests/BOJ/ucpc Team Practice 1/K.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    int n,t;
+    cin >> n >> t;
+    vector<int> a(n);
+    for(int &s:a) cin >> s;
+    
+    return 0;
+}
