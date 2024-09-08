245249d6468b534366ca064608272dbddb59ba21
pakuland3 <pakuland_1@naver.com>
2024-09-08 16:32:31 +0900

    0908

---
+++ contests/BOJ/2024 Matkor Cup Summer Fall Open Contest/A.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int n,x,y;
+    cin >> x >> y;
+    while(n--) cin >> x >> y;
+    cout << 0;
+    return 0;
+}
